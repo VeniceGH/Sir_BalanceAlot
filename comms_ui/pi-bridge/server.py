831586740cba08d13e871bbe267f42302a78a81c
Venice <vmg24@ic.ac.uk>
2026-05-21 14:41:05 +0100

    Update dashboard

--- comms_ui/pi-bridge/server.py
+++ comms_ui/pi-bridge/server.py
@@ -10,6 +10,14 @@ from robot_source import get_fake_telemetry
 
 app = FastAPI()
 
+@app.middleware("http")
+async def no_cache(request, call_next):
+    response = await call_next(request)
+    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    return response
+
 @app.on_event("startup")
 async def startup_event():
     connect_serial()
