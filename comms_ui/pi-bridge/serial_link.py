4d49d247743ddbcdf932e18f2675d89e7ff77f2d
Aileen <as7524@ic.ac.uk>
2026-06-10 13:53:46 +0100

    Latency

--- comms_ui/pi-bridge/serial_link.py
+++ comms_ui/pi-bridge/serial_link.py
@@ -62,7 +62,6 @@ def serial_reader_loop():
 
         try:
             line = ser.readline().decode('utf-8', errors='ignore').strip()
-            print(f"Serial read: {line}")
 
             if not line:
                 continue
